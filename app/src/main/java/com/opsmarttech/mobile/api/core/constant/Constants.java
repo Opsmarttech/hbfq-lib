fd626eba108163d789e04a0f8cab6ab0c94f1584
Opsmarttech <opsmarttech4github@126.com>
2019-12-25 13:33:01 +0800

    add keys

--- app/src/main/java/com/opsmarttech/mobile/api/core/constant/Constants.java
+++ app/src/main/java/com/opsmarttech/mobile/api/core/constant/Constants.java
@@ -2,9 +2,12 @@ package com.opsmarttech.mobile.api.core.constant;
 
 public class Constants {
 
-    public static final String SHAREDPREFERENCES_FILE = "hbfq-trade-route";
+    public static final String SHAREDPREFERENCES_FILE = "opsmarttech-pos-innersave";
     public static final String PRE_CREATE_ROUTE = "pre-reate-route";
     public static final String DEVICE_MEID = "device-meid";
+    public static final String CLIENT_TITLE = "client_title";
+    public static final String CLIENT_HBFQ_NUM = "client_hbfq_num";
+    public static final String CLIENT_HBFQ_PER = "client_hbfq_per";
     public static final String GATEWAY = "https://136.25.18.11";
     public static final String METHOD = "/doPosDevicePay";
     public static final String QUERY = "/doQuery";
