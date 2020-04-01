4640201efd66a9b0d2096045d7d8f18faf98fbe5
Opsmarttech <opsmarttech4github@126.com>
2020-04-01 17:33:00 +0800

    lfq tradelink fetch from server

--- app/src/main/java/com/opsmarttech/mobile/api/core/LbfqTradePayForm.java
+++ app/src/main/java/com/opsmarttech/mobile/api/core/LbfqTradePayForm.java
@@ -1,18 +1,24 @@
 package com.opsmarttech.mobile.api.core;
 
+import com.opsmarttech.mobile.api.core.http.HbfqHttpUtil;
 import com.opsmarttech.mobile.api.core.http.HbfqResponse;
 import com.opsmarttech.mobile.api.core.http.TradeParam;
 
-import java.net.HttpURLConnection;
-
 public class LbfqTradePayForm extends HbfqTradePayGeneric {
+
+//    @Override
+//    public HbfqResponse executeTradePayRequset(TradeParam param) {
+//        HbfqResponse response = new HbfqResponse();
+//        response.responseCode = HttpURLConnection.HTTP_OK;
+//        response.responseMessage = "{\"fromUrl\":\"" + param.route + "/lbf/doPay/" + param.get("totalAmount") + "/" + param.get("lbfStoreId") + "/"+ param.get("insNum") +"?tradeFrom=pos_qr\"}";
+//        return response;
+//    }
 
     @Override
     public HbfqResponse executeTradePayRequset(TradeParam param) {
-        HbfqResponse response = new HbfqResponse();
-        response.responseCode = HttpURLConnection.HTTP_OK;
-        response.responseMessage = "{\"fromUrl\":\"" + param.route + "/lbf/doPay/" + param.get("totalAmount") + "/" + param.get("lbfStoreId") + "/"+ param.get("insNum") +"?tradeFrom=pos_qr\"}";
-        return response;
+        param.put("tradeType", "form_lbf");
+        return HbfqHttpUtil.post(param);
     }
+
 
 }
